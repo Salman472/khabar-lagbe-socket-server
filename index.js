48208685313b03c3f3094ceec4a00fa67302ba8c
Salman472 <mds619820@gmail.com>
2026-05-09 12:17:14 +0600

    config socket io server

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 import express from 'express'
 import http from 'http'
 import dotenv from 'dotenv'
+import { Server } from 'socket.io'
 dotenv.config()
 
 const app = express()
 const server=http.createServer(app)
 const port =process.env.PORT || 5000
+
+const io=new Server(server, {
+    cors:{
+        origin:process.env.NEXT_BASE_URL
+    }
+})
 
 server.listen(port, ()=>{
     console.log(`app listen : http://localhost:${port}`);
